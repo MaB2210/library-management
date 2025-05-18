f5d83868138a3d9ce808cd3c3b21dddfe1398e09
MaB2210 <connect.mannbhatt@gmail.com>
2025-05-18 09:59:49 -0400

    Define Author, Book, BookDetail entities with relationships

--- src/main/java/com/example/library/library_api/model/Book.java
+++ src/main/java/com/example/library/library_api/model/Book.java
@@ -1,4 +1,34 @@
 package com.example.library.library_api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private LocalDate publishedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id",nullable = false)
+    private Author author;
+
+    @OneToOne(mappedBy = "books",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.LAZY)
+    private BookDetail detail;
+
 }
--- src/main/java/com/example/library/library_api/model/BookDetail.java
+++ src/main/java/com/example/library/library_api/model/BookDetail.java
@@ -1,4 +1,27 @@
 package com.example.library.library_api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String isbn;
+
+    private int pageCount;
+
+   @OneToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "book_id",nullable = false,unique = true)
+    private Book book;
+
 }
